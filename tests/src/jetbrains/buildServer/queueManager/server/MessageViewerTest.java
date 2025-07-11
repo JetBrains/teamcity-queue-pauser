e6f4ae154f417354d4ec3e385722a08248e73b13
Vladimir I <vladimir.ilmov@jetbrains.com>
2025-07-11 13:44:53 +0200

    TestNG7 prepare

--- tests/src/jetbrains/buildServer/queueManager/server/MessageViewerTest.java
+++ tests/src/jetbrains/buildServer/queueManager/server/MessageViewerTest.java
@@ -28,7 +28,7 @@ public class MessageViewerTest extends BaseTestCase {
 
   @BeforeMethod
   @Override
-  protected void setUp() throws Exception {
+  public void setUp() throws Exception {
     super.setUp();
     m = new Mockery() {{
       setImposteriser(ClassImposteriser.INSTANCE);
